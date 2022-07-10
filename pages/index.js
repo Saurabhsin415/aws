4c9a3dabaf8d1e00939f6238ed63c92ea99997c0
Saurabh singh <saurabhsin415@gmail.com>
2022-07-10 07:17:17 +0530

    kk

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,19 @@ export default function Home() {
 </div>
 </div>
 
+<div className='content-wrap1'>
+  <img src="https://html.themexriver.com/Saasio/assets/img/port-img-31/cta-bg-shape.png"  className='img1' style={{'height':'120px'}}/>
+  {/* <div></div> */}
+ 
+  <div className="text-center py-3 text-family">
+  Satta Matka Kalyan Main Mumbai Fastest Results
+Get Kalyan Matka Main Mumbai Satta Matka Market Results Fastest Live Update. Get All Kalyan,Main Mumbai,Madhur Matka,Rajdhani,Milan,Madhur Matka And Time Bazar Jodi Penal Panel Patti Panna Charts For Free . All Matka Guessing.With Best Guessers , Online Old Charts , Satta Matka Number Software Links, Online Charts List Pdf Download And Top Matka Guessing Free Number Provided By Sattamatka143 Professor And Master Dr Admin Sir.
+Other Special Features Include 220 Patti Satta Weekly Matka Jodi Chart With Direct Access To Guessing Form Of Experts Tricks Access Via Website
+
+</div>
+</div>
+
+.
     </>
     
   );
