1772d4157bc8890d3e9008e328fc9d0e71dfcba5
Saurabh singh <saurabhsin415@gmail.com>
2022-07-10 07:10:18 +0530

    ff

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   {/* <img src="https://html.themexriver.com/Saasio/assets/img/port-img-31/cta-bg-shape.png" className='img1'/> */}
   {/* <div></div> */}
  
-  <div class="text-center py-3 text-family">
+  <div className="text-center py-3 text-family">
   <span className='text-color1'>|</span>  <Link href={'https://sattamatka.rocks/'}><a className='text-color2 font-semibold mr-2'>Sattamatka</a></Link>
    <span className='text-color1'>|</span>  <Link href={'https://sattamatka.rocks/'}><a className='text-color2 font-semibold mr-2'>Dpboss</a></Link>
    <span className='text-color1'>|</span>  <Link href={'https://sattamatka.rocks/'}><a className='text-color2 font-semibold mr-2'>Sattamatkà</a></Link>
