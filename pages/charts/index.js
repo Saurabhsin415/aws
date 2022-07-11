229835b9ab833439d734fe027fe29a1409f7fd5a
Saurabh singh <saurabhsin415@gmail.com>
2022-07-11 22:53:39 +0530

    nn

--- pages/charts/index.js
+++ pages/charts/index.js
@@ -21,7 +21,7 @@ export default function ChartIndex() {
         {data &&
           data.data.map((item,index) => (
             <div className='content-wrap1 text-center text-family result-div div_link' key={index}>
-            <Link href={`${item.chart_slug.toLowerCase()+'-jodi-chart'}`}>
+            <Link href='/'>
   <a className='link'>{`${item.chartname.toLowerCase()}`}</a>
  </Link>
  </div>
@@ -35,7 +35,7 @@ export default function ChartIndex() {
           data.data.map((item,index) => (
 
             <div className='content-wrap1 text-center text-family result-div div_link' key={index}>
- <Link href={`${item.chart_slug.toLowerCase()+'-panel-chart'}`}>
+ <Link href='/'>
   <a className='link'>{`${item.chartname.toLowerCase()}`}</a>
  </Link>
  </div>
