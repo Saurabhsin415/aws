5131ddfb75547c72deaf28cc4b49cddf9f806e55
Saurabh singh <saurabhsin415@gmail.com>
2022-08-19 02:11:26 +0530

    nm

--- pages/history-of-sattamatka.js
+++ pages/history-of-sattamatka.js
@@ -17,7 +17,7 @@ to the Bombay Cotton Exchange from the New York Cotton Exchange, via teleprinter
 keep the matka business alive. A Sindhi migrant from Karachi, Pakistan, Ratan Khatri introduced the idea of declaring 
 opening and closing rates of imaginary products and playing cards. Numbers would be written on pieces of paper and put 
 into a matka, a large earthen pitcher. One person would then draw a chit and declare the winning numbers. Over the years, 
-the practice changed, so that three numbers were drawn from a pack of playing cards, but the name "matka" was kept.</p>
+the practice changed, so that three numbers were drawn from a pack of playing cards, but the name `matka` was kept.</p>
 
 <p>In 1962, Kalyanji Bhagat started the Worli matka. Ratan Khatri then introduced the New Worli matka in 1964, 
 with slight modifications to the rules of the game with odds that were more favourable to the public. 
@@ -29,7 +29,7 @@ it began to be called Main Ratan matka.</p>
 their shops in and around the mill areas, predominantly located in Parel in Central Mumbai and Kalbadevi in South Mumbai.</p>
 
 <p>The decades of 1980s and 1990s saw the matka business reach its peak. Betting volumes in excess of Rs. 500 crore would be
-laid every month. The Mumbai Police’s massive crackdown on the matka dens forced dealers to shift their hideouts to the city’s outskirts.
+laid every month. The Mumbai Police`s massive crackdown on the matka dens forced dealers to shift their hideouts to the city`’`s outskirts.
 Many of them moved to Gujarat, Rajasthan and other states. With no major source of betting in the city, many punters were attracted to other
 forms of gambling such as online and zhatpat lotteries. Meanwhile, some rich punters began to explore betting on cricket matches.</p>
 
