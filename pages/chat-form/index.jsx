7fe67c75f255ec0cfa81515d114facd38169eb7e
Saurabh singh <saurabhsin415@gmail.com>
2022-08-22 18:13:11 +0530

    h

--- pages/chat-form/index.jsx
+++ pages/chat-form/index.jsx
@@ -26,6 +26,7 @@ import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import TextStyle from '@tiptap/extension-text-style'
 import { EditorContent, useEditor } from '@tiptap/react'
+import {Helmet} from "react-helmet";
 export default function Guessingforum() {
 //  console.log(token);
 const { result, errors } =UserInfo();
@@ -257,7 +258,11 @@ result.then(response=>{
 const {loadingMore,isReachedEnd,error,size,setSize,paginatedPost}=usePagination(`chat-forum?orginal=${orgin}&search=${search}`);
   return (
     <>
+  <Helmet>
+ <title>SATTAMATKA DPBOSS 143 GUESSING FORUM</title>
+ <meta name="description" content="DPBOSS GUESSING FORUM - SATTAMATKA 143 GUESSING FORUM , DP BOSS , SATTAMATKà , SATTA MATKA , 143 SATTAMATKA , 143 GUESSING FORUM , DPBOSS 143 GUESSING FORUM "/><meta name="keywords" content="DPBOSS 143 GUESSING FORUM | SATTAMATKà | DPBOSS | KALYAN MATKA TIPS | SATTA MATKA | MATKA.COM | MATKA PANA JODI TODAY | BATTA SATKA | MATKA PATTI JODI NUMBER | MATKA RESULTS | MATKA CHART | MATKA JODI | FULL RATE GAME | MATKA GAME | ALL MATKA RESULT LIVE ONLINE | MATKA RESULT | KALYAN MATKA RESULT | DPBOSS MATKA 143 | MAIN MATKA"/>
  
+ </Helmet>
 <div className="text-center py-3 text-family1 guessing-forum">
 <h3 style={{'marginBottom':'0px'}}>Chat Forum</h3> 
  {/* <div className='text-color1'>Date:10/07/2022</div> */}
